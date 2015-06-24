9e406fd21da8cbf4c24f0501b077dfa6ac93d361
rohit <shobhittyagi0791@gmail.com>
2015-06-24 21:33:35 +0530

    check change

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,9 @@ class StaticPagesController < ApplicationController
   def home
     @micropost = current_user.microposts.build if logged_in?
     @feed_items = current_user.feed.paginate(page: params[:page]) if logged_in?
-  end
+  
+#djfgsdjfgsj
+end
 
   def help
   end
